65c84253ca101ad56fb4a7776a53ec5d92f07b9f
Hoangducc1997 <Hoangducc1997@gmail.com>
2024-03-24 12:58:56 +0700

    new

--- Assets/Scripts/UI/GameManagerLevel.cs
+++ Assets/Scripts/UI/GameManagerLevel.cs
@@ -59,4 +59,90 @@ public class GameManagerLevel : MonoBehaviour
         Time.timeScale = 1;
     }
 
+
+    //Button sound
+    [SerializeField] private Slider _musicSliderMenu, _musicSliderLevel, _sfxSlider;
+    public void MuteMenuMusic()
+    {
+        AudioManager.Instance.MuteMenuMusic();
+        // SaveAudioSettings();
+    }
+
+    public void UnmuteMenuMusic()
+    {
+        AudioManager.Instance.UnmuteMenuMusic();
+        //SaveAudioSettings();
+    }
+
+    public void MuteLevelMusic()
+    {
+        AudioManager.Instance.MuteLevelMusic();
+        //SaveAudioSettings();
+    }
+
+    public void UnmuteLevelMusic()
+    {
+        AudioManager.Instance.UnmuteLevelMusic();
+        // SaveAudioSettings();
+    }
+
+    public void MuteSFX()
+    {
+        AudioManager.Instance.MuteSFX();
+        // SaveAudioSettings();
+    }
+
+    public void UnmuteSFX()
+    {
+        AudioManager.Instance.UnmuteSFX();
+        //SaveAudioSettings();
+    }
+
+    // Các hàm kiểm tra trạng thái âm thanh
+    private bool IsMenuMusicMuted()
+    {
+        return AudioManager.Instance.IsMenuMusicMuted();
+    }
+
+    private bool IsLevelMusicMuted()
+    {
+        return AudioManager.Instance.IsLevelMusicMuted();
+    }
+
+    private bool IsSfxMuted()
+    {
+        return AudioManager.Instance.IsSfxMuted();
+    }
+
+    public void MusicVolumeMenu()
+    {
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.musicVolumeMenu(_musicSliderMenu.value);
+            // SaveAudioSettings();
+        }
+        else
+        {
+            Debug.LogWarning("AudioManager.Instance is null. Unable to set music volume.");
+        }
+    }
+
+    public void MusicVolumeLevel()
+    {
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.musicVolumeLevel(_musicSliderLevel.value);
+            // SaveAudioSettings();
+        }
+    }
+
+    public void SfxVolume()
+    {
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.sfxVolume(_sfxSlider.value);
+            // SaveAudioSettings();
+        }
+    }
+
 }
--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -4,145 +4,127 @@ using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    public void openLevel(int level)
-    {
-        AudioManager.Instance.PlaySFX("ChooseLevel");
-        string levelName = "Level " + level;
-        SceneManager.LoadScene(levelName);
-    }
-    public void Home()
-    {
-
-        SceneManager.LoadScene("Menu");
-        Time.timeScale = 1;
-    }
-
-    public void quitGame()
-    {
-        Application.Quit();
-        Debug.Log("Quit!");
-    }
-
-
-    //public Slider _musicSliderMenu, _musicSliderLevel, _sfxSlider;
-
-
-    ////private void Start()
-    ////{
-    ////    LoadAudioSettings();
-    ////}
-
     
-
-    ////private void LoadAudioSettings()
-    ////{
-       
-    ////    if (PlayerGameData.Instance != null)
-    ////    {
-    ////        _musicSliderMenu.value = PlayerGameData.Instance.musicVolumeMenu;
-    ////        _musicSliderLevel.value = PlayerGameData.Instance.musicVolumeLevel;
-    ////        _sfxSlider.value = PlayerGameData.Instance.sfxVolume;
-           
-    ////    }
-    ////    else
-    ////    {
-    ////        PlayerGameData.Instance = SaveManager.Load();
-    ////    }
-    ////}
+   
+    public Slider _musicSliderMenu, _musicSliderLevel, _sfxSlider;
 
 
-
-
-    ////private void SaveAudioSettings()
-    ////{
-    ////    PlayerGameData.Instance.SaveSetting(
-    ////        _musicSliderMenu.value,
-    ////        _musicSliderLevel.value,
-    ////        _sfxSlider.value
-    ////    );
-    ////}
-
-
-    //public void MuteMenuMusic()
+    //private void Start()
     //{
-    //    AudioManager.Instance.MuteMenuMusic();
-    //   // SaveAudioSettings();
+    //    LoadAudioSettings();
     //}
 
-    //public void UnmuteMenuMusic()
-    //{
-    //    AudioManager.Instance.UnmuteMenuMusic();
-    //    //SaveAudioSettings();
-    //}
 
-    //public void MuteLevelMusic()
-    //{
-    //    AudioManager.Instance.MuteLevelMusic();
-    //    //SaveAudioSettings();
-    //}
 
-    //public void UnmuteLevelMusic()
+    //private void LoadAudioSettings()
     //{
-    //    AudioManager.Instance.UnmuteLevelMusic();
-    //   // SaveAudioSettings();
-    //}
 
-    //public void MuteSFX()
-    //{
-    //    AudioManager.Instance.MuteSFX();
-    //   // SaveAudioSettings();
-    //}
-
-    //public void UnmuteSFX()
-    //{
-    //    AudioManager.Instance.UnmuteSFX();
-    //    //SaveAudioSettings();
-    //}
-
-    //// Các hàm kiểm tra trạng thái âm thanh
-    //private bool IsMenuMusicMuted()
-    //{
-    //    return AudioManager.Instance.IsMenuMusicMuted();
-    //}
-
-    //private bool IsLevelMusicMuted()
-    //{
-    //    return AudioManager.Instance.IsLevelMusicMuted();
-    //}
-
-    //private bool IsSfxMuted()
-    //{
-    //    return AudioManager.Instance.IsSfxMuted();
-    //}
-
-    //public void MusicVolumeMenu()
-    //{
-    //    if (AudioManager.Instance != null)
+    //    if (PlayerGameData.Instance != null)
     //    {
-    //        AudioManager.Instance.musicVolumeMenu(_musicSliderMenu.value);
-    //       // SaveAudioSettings();
+    //        _musicSliderMenu.value = PlayerGameData.Instance.musicVolumeMenu;
+    //        _musicSliderLevel.value = PlayerGameData.Instance.musicVolumeLevel;
+    //        _sfxSlider.value = PlayerGameData.Instance.sfxVolume;
+
     //    }
     //    else
     //    {
-    //        Debug.LogWarning("AudioManager.Instance is null. Unable to set music volume.");
+    //        PlayerGameData.Instance = SaveManager.Load();
     //    }
     //}
 
-    //public void MusicVolumeLevel()
+
+
+
+    //private void SaveAudioSettings()
     //{
-    //    if (AudioManager.Instance != null)
-    //    {
-    //        AudioManager.Instance.musicVolumeLevel(_musicSliderLevel.value);
-    //       // SaveAudioSettings();
-    //    }
+    //    PlayerGameData.Instance.SaveSetting(
+    //        _musicSliderMenu.value,
+    //        _musicSliderLevel.value,
+    //        _sfxSlider.value
+    //    );
     //}
 
-    //public void SfxVolume()
-    //{
-    //    if (AudioManager.Instance != null)
-    //    {
-    //        AudioManager.Instance.sfxVolume(_sfxSlider.value);
-    //       // SaveAudioSettings();
-    //    }
-    //}
+
+    public void MuteMenuMusic()
+    {
+        AudioManager.Instance.MuteMenuMusic();
+        // SaveAudioSettings();
+    }
+
+    public void UnmuteMenuMusic()
+    {
+        AudioManager.Instance.UnmuteMenuMusic();
+        //SaveAudioSettings();
+    }
+
+    public void MuteLevelMusic()
+    {
+        AudioManager.Instance.MuteLevelMusic();
+        //SaveAudioSettings();
+    }
+
+    public void UnmuteLevelMusic()
+    {
+        AudioManager.Instance.UnmuteLevelMusic();
+        // SaveAudioSettings();
+    }
+
+    public void MuteSFX()
+    {
+        AudioManager.Instance.MuteSFX();
+        // SaveAudioSettings();
+    }
+
+    public void UnmuteSFX()
+    {
+        AudioManager.Instance.UnmuteSFX();
+        //SaveAudioSettings();
+    }
+
+    // Các hàm kiểm tra trạng thái âm thanh
+    private bool IsMenuMusicMuted()
+    {
+        return AudioManager.Instance.IsMenuMusicMuted();
+    }
+
+    private bool IsLevelMusicMuted()
+    {
+        return AudioManager.Instance.IsLevelMusicMuted();
+    }
+
+    private bool IsSfxMuted()
+    {
+        return AudioManager.Instance.IsSfxMuted();
+    }
+
+    public void MusicVolumeMenu()
+    {
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.musicVolumeMenu(_musicSliderMenu.value);
+            // SaveAudioSettings();
+        }
+        else
+        {
+            Debug.LogWarning("AudioManager.Instance is null. Unable to set music volume.");
+        }
+    }
+
+    public void MusicVolumeLevel()
+    {
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.musicVolumeLevel(_musicSliderLevel.value);
+            // SaveAudioSettings();
+        }
+    }
+
+    public void SfxVolume()
+    {
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.sfxVolume(_sfxSlider.value);
+            // SaveAudioSettings();
+        }
+    }
 }
